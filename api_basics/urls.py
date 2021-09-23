5e7ef9ee9a2139cb7d8c9fb3679561641a28815f
Tawfiq-Abu <tawfiqabubakr7@gmail.com>
2021-09-23 21:27:59 +0000

    done with class based views

--- api_basics/urls.py
+++ api_basics/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('article/',views.article_list),
-    path('detail/<int:pk>/',views.article_detail)
+    # path('article/',views.article_list),
+    path('article/',views.ArticleAPIView.as_view()),
+    # path('detail/<int:pk>/',views.article_detail)
+    path('detail/<int:id>/',views.Articledetail.as_view())
 ]
